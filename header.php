80f634679ba210dafd34a79ebbdf39cca2946c9c
Anthony Polanowski <Polanowski@users.noreply.github.com>
2019-02-06 10:55:35 -0700

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,15 @@
-<ul class="nav">
-  <li>Home</li>
-  <li>About</li>
-  <li>Projects</li>
-</ul>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Title of the document</title>
+  </head>
+  <body>
+    <section class="header">
+    <ul class="nav">
+      <li>Home</li>
+      <li>About</li>
+      <li>Projects</li>
+    </ul>
+    </section>
+  </body>
+</html>
