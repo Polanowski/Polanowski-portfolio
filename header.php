16a9657d5aee5dd57e7e71917aac502ac9c9665a
Anthony Polanowski <Polanowski@users.noreply.github.com>
2019-02-06 14:23:12 -0700

    Update header.php
    Changing the header

--- header.php
+++ header.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Title of the document</title>
-  </head>
-  <body>
-    <section class="header">
+<?php
+    echo "<section class="header">
     <ul class="nav">
       <li>Home</li>
       <li>About</li>
       <li>Projects</li>
     </ul>
-    </section>
-  </body>
-</html>
+    </section>";
+?>
