43761b7c3d246ba0f04c9fd787b0dcd41bebb911
Anthony Polanowski <Polanowski@users.noreply.github.com>
2019-02-20 14:04:36 -0700

    Update header.php

--- header.php
+++ header.php
@@ -1,14 +1,5 @@
-<?php
- /*This is a normal comment in php
- for multiple lines*/
-//This works too
-?>
-<?php
-    $header = '<section class="header">
-    <ul class="nav">
-      <li>Home</li>
-      <li>About</li>
-      <li>Projects</li>
-    </ul>
-    </section>';
-?>
+<ul class="nav">
+  <li>Home</li>
+  <li>About</li>
+  <li>Projects</li>
+</ul>
